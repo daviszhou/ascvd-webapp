bdc6e4f4f8d91ab7eee32414027c72f228abbb98
daviszhou <zhou.davis@outlook.com>
2015-12-31 22:45:16 -0500

    Debugging Crash

--- __init__.py
+++ __init__.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
-	return "Hello World!"
+def hello():
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
-	app.run(debug=True)
