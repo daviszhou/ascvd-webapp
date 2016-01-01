b3e181d64be5a291f45a54f1e00f3feb0dd4537a
daviszhou <zhou.davis@outlook.com>
2015-12-31 23:43:19 -0500

    Add html template + logic + variables

--- __init__.py
+++ __init__.py
@@ -1,12 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def homepage():
-	return render_template("index.html")
 
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+	title = 'Epic Tutorial'
+	paragraph = ['While the deployment is happening, you may see a syntax error during the install for gunicorn about invalid syntax for the line yield from self.wsgi.close(). That error can be ignored.', 'While the deployment is happening, you may see a syntax error during the install for gunicorn about invalid syntax for the line yield from self.wsgi.close(). That error can be ignored.']
+
+	return render_template('index.html', title=title, paragraph=paragraph)
+
+@app.route('/about')
+def aboutpage():
+
+	title = "About the site"
+	paragraph = ["While the deployment is happening, you may see a syntax error during the install for gunicorn about invalid syntax for the line yield from self.wsgi.close(). That error can be ignored.', 'While the deployment is happening, you may see a syntax error during the install for gunicorn about invalid syntax for the line yield from self.wsgi.close()."]
+
+	page_type = 'about'
+	return render_template('index.html', title=title, paragraph=paragraph, page_type=page_type)
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5020))
 	app.run(host='0.0.0.0', port=port)
 	app.run(debug=True)
