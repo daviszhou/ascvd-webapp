76929dda608e897feeab6b67a91843d2312780e1
daviszhou <zhou.davis@outlook.com>
2015-12-31 22:38:15 -0500

    Add html page

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "Hello world!"
+def homepage():
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
