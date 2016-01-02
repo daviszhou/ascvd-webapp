c91b38e732656c2f96c3ee78ed31f18a6e331886
daviszhou <zhou.davis@outlook.com>
2016-01-02 13:38:33 -0500

    Debugg + fix css override

--- __init__.py
+++ __init__.py
@@ -19,6 +19,6 @@ def patientpage():
 	return render_template('index.html', description=description)
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5010))
 	app.run(host='0.0.0.0', port=port)
 	app.run(debug=True)
